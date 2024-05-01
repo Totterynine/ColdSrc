13c1f0cfd12abddad75dbce4afffeac33033a53f
Tottery <Totterynine@gmail.com>
2024-05-01 19:38:48 +0500

    Basic usage of the rendersystem

--- game/game_app.cpp
+++ game/game_app.cpp
@@ -11,6 +11,8 @@ class GameApp : public IApplication, public IModule
 {
 public:
 
+    double CurrentTime = 0.0;
+
 	GameApp() : IModule("GameApp") {}
 
     virtual void *GetInterface() { return static_cast<IApplication *>( this ); }
@@ -34,10 +36,10 @@ public:
 
         void *hwnd = SDL_GetProperty( SDL_GetWindowProperties( window ), SDL_PROP_WINDOW_WIN32_HWND_POINTER, NULL );
 
-        if ( !rendersys->CreateRenderer() )
+        if ( !rendersys->Create() )
             return false;
 
-        rendersys->AttachWindow( hwnd );
+        rendersys->AttachWindow( &hwnd, 1280, 720 );
 
         return true;
     }
@@ -70,12 +72,32 @@ public:
     // Run the application logic before frame begins.
     virtual void Simulate( float dt )
     {
-
+        CurrentTime += dt;
     }
 
     // Runs the process of rendering the frame, then it presenting on selected surface.
     virtual void Frame()
     {
+        ColorFloat clearClr = { };
+        clearClr.r = sin( CurrentTime / 2 ) + 1.0;
+        clearClr.g = sin( CurrentTime + ( 3.1415926 / 2.0 ) ) + 1;
+        clearClr.b = sin( CurrentTime + 3.1415926 ) + 1;
+        clearClr.a = 1.0;
+
+        clearClr.r /= 2.0;
+        clearClr.g /= 2.0;
+        clearClr.b /= 2.0;
+
+        rendersys->BeginRendering();
+
+        rendersys->SetViewport( { 0,0,1280,720 } );
+
+        rendersys->SetClearColor( clearClr );
+        rendersys->ClearColor();
+
+        rendersys->EndRendering();
+
+        rendersys->Present();
 
     }
 
